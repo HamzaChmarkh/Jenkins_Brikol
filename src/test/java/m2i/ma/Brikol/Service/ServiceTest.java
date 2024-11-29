7039550d55bed8bf8f3da926c0895b16de83e5f5
Abbal Ilyas <ilyas.abbal10@gmail.com>
2024-11-29 11:11:32 +0100

    Ajout du fichier .gitignore

---
+++ src/test/java/m2i/ma/Brikol/Service/ServiceTest.java
@@ -0,0 +1,74 @@
+package m2i.ma.Brikol.Service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getTitre() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void getPrix() {
+    }
+
+    @Test
+    void getPathImage() {
+    }
+
+    @Test
+    void getFreelancer() {
+    }
+
+    @Test
+    void getCategorie() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void setTitre() {
+    }
+
+    @Test
+    void setDescription() {
+    }
+
+    @Test
+    void setPrix() {
+    }
+
+    @Test
+    void setPathImage() {
+    }
+
+    @Test
+    void setFreelancer() {
+    }
+
+    @Test
+    void setCategorie() {
+    }
+}
