1c0a0bb6efd6e20adc4a8dd8d63c29dbb0b2927f
Abbal Ilyas <ilyas.abbal10@gmail.com>
2024-11-30 12:37:20 +0100

    fix returns in categorie freelnacer ans service

--- src/main/java/m2i/ma/Brikol/Freelancer/FreelancerDto.java
+++ src/main/java/m2i/ma/Brikol/Freelancer/FreelancerDto.java
@@ -28,20 +28,4 @@ public class FreelancerDto {
     private String address;
 
 
-    public FreelancerDto(String image, String username, String name, Long id, String email, List<Service> servicesProposes, String nickName, String publicEmail, String description, String phoneNumber, String region, String city, String zip, String address) {
-        this.image = image;
-        this.username = username;
-        this.name = name;
-        this.id = id;
-        this.email = email;
-        this.servicesProposes = servicesProposes;
-        this.nickName = nickName;
-        this.publicEmail = publicEmail;
-        this.description = description;
-        this.phoneNumber = phoneNumber;
-        this.region = region;
-        this.city = city;
-        this.zip = zip;
-        this.address = address;
-    }
 }
